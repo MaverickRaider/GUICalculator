5ea4d4d2018dd8aae1f76f2f9985a294f002355a
MichaelHickey <michaelhickey319@gmail.com>
2021-07-03 13:02:10 -0400

    update

---
+++ src/main/java/ucf/assignment/CalculatorController.java
@@ -0,0 +1,89 @@
+package ucf.assignment;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class CalculatorController {
+    @FXML
+    private TextField resultDisplay;
+
+    @FXML
+    public void oneButtonClicked(ActionEvent actionEvent) {
+        append1();
+    }
+
+    @FXML
+    public void twoButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void threeButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void addButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void fourButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void fiveButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void sixButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void subButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void sevenButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void eightButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void nineButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void multiButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void zeroButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void divButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void enterButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void deciButtonClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void clearButtonClicked(ActionEvent actionEvent) {
+        clearDisplay();
+    }
+
+    public void clearDisplay() {
+        // set the value of resultDisplay to ""
+    }
+
+    public void append1() {
+        // append a "1" to the current value of the resultDisplay
+    }
+}
---
+++ src/test/java/ucf/assignment/CalculatorControllerTest.java
@@ -0,0 +1,84 @@
+package ucf.assignment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorControllerTest {
+
+    @Test
+    void clearDisplay() {
+        // make a new calculator controller
+        // initialize the resultDisplay to some non-blank value
+        // call the clearDisplay function
+        // get the value resultDisplay
+        // assert that the new value is blank
+    }
+
+    @Test
+    void append1() {
+        // click the 1 button
+        // get the value in resultDisplay
+        // assert that resultDisplay is 1
+    }
+
+    @Test
+    void twoButtonClicked() {
+    }
+
+    @Test
+    void threeButtonClicked() {
+    }
+
+    @Test
+    void addButtonClicked() {
+    }
+
+    @Test
+    void fourButtonClicked() {
+    }
+
+    @Test
+    void fiveButtonClicked() {
+    }
+
+    @Test
+    void sixButtonClicked() {
+    }
+
+    @Test
+    void subButtonClicked() {
+    }
+
+    @Test
+    void sevenButtonClicked() {
+    }
+
+    @Test
+    void eightButtonClicked() {
+    }
+
+    @Test
+    void nineButtonClicked() {
+    }
+
+    @Test
+    void multiButtonClicked() {
+    }
+
+    @Test
+    void zeroButtonClicked() {
+    }
+
+    @Test
+    void divButtonClicked() {
+    }
+
+    @Test
+    void enterButtonClicked() {
+    }
+
+    @Test
+    void deciButtonClicked() {
+    }
+}
